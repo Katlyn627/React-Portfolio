dc7e4d2c94825b9ef2833573192bdbc9b4c2aaf9
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-03 05:47:17 -0700

    updated pictures

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
             <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               <a href={project} target="_blank" className="button btn github-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
+               <a href={project} target="_blank" className="button btn linked-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
                <a href={github} target="_blank" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
