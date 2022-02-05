141cff825755e9d4dea27e3c2d3b8e60bc0c60f6
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-04 18:26:27 -0700

    updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,9 +5,11 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
+      // eslint-disable-next-line
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
+      // eslint-disable-next-line
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
