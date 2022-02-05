1c5591736ea87ddb2ea1f199ea1179cfb26ae508
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-04 22:58:12 -0700

    updates to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="cobweb"  bg={true} />
+      <ParticlesBg num={200} type="circle" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
