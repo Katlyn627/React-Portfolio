83c1ceacac0e560e2a953af532870ab04a7109e3
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-12 16:13:07 -0700

    fixing bugs

--- src/Components/pages/data/ProjectCards.js
+++ src/Components/pages/data/ProjectCards.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-
-        {
-          "id"; 1,
-          "cardname"; "Marvel Comic App",
-          "image"; "/src/components/images/signup page.jpg",
-          "livelink"; "",
-          "codelink"; ""
-        }
-        {
-            "id"; 2,
-            "cardname"; "Bright Ideas",
-            "image"; "/src/components/images/login-page.png",
-            "livelink"; "",
-            "codelink"; ""
-        }
-        {
-            "id"; 3,
-            "cardname"; "Couch Camp",
-            "image"; "/src/components/images/couch camp logo.png",
-            "livelink"; "",
-            "codelink"; ""
-        }
-        {
-            "id"; 4,
-            "cardname"; "Coding Quiz",
-            "image"; "/src/components/images/code-quiz.png",
-            "livelink"; "",
-            "codelink"; "https://github.com/Katlyn627/Coding-Quiz-Challenge "
-        }
-        {
-            "id"; 5,
-            "cardname"; "Work Day Planner",
-            "image"; "",
-            "livelink"; "",
-            "codelink"; ""
-        }
-        {
-            "id"; 6,
-            "cardname"; "Weather Dashboard",
-            "image"; "",
-            "livelink"; "",
-            "codelink"; "",
-        }
-    
-=======
 // eslint-disable-next-line no-unused-expressions
 [
     {
@@ -89,4 +43,3 @@
         "codelink": ""
     }
 ]    
->>>>>>> 3ab97b5524f9e5a79085689abff518d7b2e73fde
