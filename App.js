1363d2644b2105eb399d9269bc8f434445d02376
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-21 15:06:54 -0700

    testing

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./Components/NavTabs";
+import Home from "./Components/pages/Home";
+import Projects from "./Components/pages/Projects";
+import Contact from "./Components/pages/Contact";
+import HeroImage from "./Components/HeroImage";
+import FooterInfo from "./Components/FooterInfo"
+
+function App() {
+  return (
+    <Router basename={ process.env.PUBLIC_URL }>
+      <div>
+        <HeroImage/>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Projects" component={Projects} />
+        <Route path="/Contact" component={Contact} />
+        <FooterInfo/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ HeroImage.js
@@ -0,0 +1,13 @@
+//header will go here
+import "./style.css";
+import React from "react";
+import Hero from "../assets/linked in background.png"
+
+function HeroImage() {
+    return (
+        <img className="hero" src={Hero} alt="Katlyn Boches" />
+
+    );
+}
+
+export default HeroImage;
