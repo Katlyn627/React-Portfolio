43cb30363db3c8f3910e81095d761021ff3e0616
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-03-06 23:04:44 -0700

    updated icons

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/Katlyn627"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/katlyn-boches-20110732/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        <a href="https://katlyn627.github.io/react-portfolio/"><img src="https://img.icons8.com/ios-glyphs/48/ffffff/trilby.png" alt="KatlynBoches.com" className="icon"/></a>
+        <a href="https://katlyn627.github.io/react-portfolio/"><img src="https://user-images.githubusercontent.com/86095070/156977005-fa0d377e-883a-49af-b3e1-85d12658e73e.png" alt="KatlynBoches.com" className="icon"/></a>
     </footer>
     )
 }
