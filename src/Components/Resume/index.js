91887d55307420f2f48beba1b898d418140f3b77
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-03-06 21:23:57 -0700

    fixed filing structure and styling

---
+++ src/Components/Resume/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Katlyn Boches</h2>
+          <ul>
+            <li>
+              Core competencies in customer service and management, full-stack web
+              development, data structures, and managing teams.
+            </li>
+          </ul>
+
+          <p class="mt-5">
+            <a href="https://www.linkedin.com/in/katlyn-boches-20110732/">
+              <img
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="inkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href="https://github.com/Katlyn627/react-portfolio/files/8194785/Resume.2022.1.docx"
+            class="link"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title">Proficiencies</h2>
+          <ul>
+            <li>
+            HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js, IndexedDB,<br />
+            Node.js, Express.js, SQL, Sequelize, MongoDB, Mongoose, NoSQL, APIs, Templating.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
