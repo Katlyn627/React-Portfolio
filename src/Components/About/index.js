8ad9e005357252276a44d4a60d7a1be10ae4b26d
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-03-06 21:38:53 -0700

    re-doing images

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -7,11 +7,10 @@ function About() {
             <h2 class="top-title">Katlyn Boches</h2>
             <hr></hr>
             <div>
-                <img class="mb-5" id="avatar" src="../../assets/headshot.jpg" alt="Katlyn Boches" />
+                <img class="mb-5" id="avatar" src="https://user-images.githubusercontent.com/86095070/156967901-33a742df-47f4-40c7-bc50-eeb5f1634e8d.jpg" alt="Katlyn Boches" />
 
                 <p>
-                I am a Colorado native and current student at the University of Denver enrolled in their full-stack coding boot-camp. In my free time I enjoy learning new skills such as American Sign Language and the development of video games, working on personal goals
-      and helping my community. I enjoy hiking, camping and paddle-boarding. I enjoy spending time with family and friends playing board games like chess, and card games like cribbage and poker. After graduating from culinary school with
+                I am a Colorado native and graduate student of the University of Denver from their full-stack coding boot-camp. After graduating from culinary school with
       a Bachelor' s of Science in Food Service Management I decided on broadening my horizons and seeing the world. I am a travel enthusiast with skills in business, marketing, and computer programming. I spent the last 10 years traveling
       both internationally and around the United States. This enabled me to get a vast amount experience in different jobs as well as seeing different cultures and communities. It has given my a wealth of knowledge in both business and customer
       service as well as hospitality. I am eager to get started with my career in this field and am looking to using my skills to start a successful career in the Computer Programming and Technology Industry.
