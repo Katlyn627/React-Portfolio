e06bde34f64f699c6c467fd07224296f8810718b
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-03-06 21:16:58 -0700

    fixing file structure and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "../src/Components/NavTabs";
-import Home from "../src/Components/pages/Home";
-import Projects from "../src/Components/pages/Projects";
-import Contact from "../src/Components/pages/Contact";
-import HeroImage from "../src/Components/HeroImage";
-import FooterInfo from "../src/Components/FooterInfo"
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <Router basename={ process.env.PUBLIC_URL }>
-      <div>
-        <HeroImage/>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Projects" component={Projects} />
-        <Route path="/Contact" component={Contact} />
-        <FooterInfo/>
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <Footer/>
+    </div>
   );
 }
 
