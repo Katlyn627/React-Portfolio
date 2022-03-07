69332f953ccddc3c2a53105748a892f1df084ca8
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-03-06 21:46:34 -0700

    updating images and errors

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {   
     return (
     <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/Katlyn627"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/katlyn-boches-20110732/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        <a href="https://www.github.Katlyn627.com"><img src="https://img.icons8.com/ios-glyphs/48/ffffff/trilby.png" alt="KatlynBoches.com" className="icon"/></a>
+        <a href="https://katlyn627.github.io/react-portfolio/"><img src="https://img.icons8.com/ios-glyphs/48/ffffff/trilby.png" alt="KatlynBoches.com" className="icon"/></a>
     </footer>
     )
 }
